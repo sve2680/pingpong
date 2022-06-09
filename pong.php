108679e0d38d7501e16961fc8a7367918481b44d
sergiovb26 <sve2680@gmail.com>
2022-06-09 18:57:39 +0200

    Versió 0: versió inicial

---
+++ pong.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $valorPing=0;
+    $valorPong=0;
+
+    
+    if(isset($_GET['valorPing'])) {
+        $valorPing=$_GET['valorPing']+1;
+    }
+    elseif(isset($_POST['valorPong'])) {
+        $valorPong=$_POST['valorPong']+1;
+    }
+
+
+?>
+
+    <div>
+        <form action="ping.php" method="GET">
+            <input type="text" name="valorPing" value =<?php echo $valorPing?>>
+            <input type="submit" value="Ping??">
+        </form>
+
+        <form action="pong.php" method="POST">
+            <input type="text" name="valorPong" value =<?php echo $valorPong?>>
+            <input type="submit" value="Pong??">
+        </form>
+    </div>
+</body>
+</html>
